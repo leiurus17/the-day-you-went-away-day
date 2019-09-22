65ee5a7ddea1dc4e70d122646d27405aa9a09a78
Marlon Monzon <martianworm17@gmail.com>
2019-09-22 13:41:36 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        findDayYouWentAwayDays();
+    }
+
+    private static void findDayYouWentAwayDays() {
+
+        for (int x = 1970; x <= 2100; x++) {
+
+            LocalDate septemberTwentySecondSunday = LocalDate.of(x, 9, 22);
+
+            if (septemberTwentySecondSunday.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
+                System.out.println("Year: " + x + " has a Day You Went Away day!");
+            }
+        }
+    }
+}
